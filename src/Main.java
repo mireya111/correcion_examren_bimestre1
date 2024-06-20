78eaf8d271efcc33557d1aa39d5e693fb34a9aed
Mireya Garcia <mireya.garcia@epn.edu.ec>
2024-06-20 08:00:48 -0500

    version 2 - main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import sistemaTransporte.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Objetos con valores nulos
+        pasajeros pasajero = new pasajeros();
+        pasajeros pasajero2 = new pasajeros();
+        pasajeros pasajero3 = new pasajeros();
+        pasajeros pasajero4 = new pasajeros();
+        pasajeros pasajero5 = new pasajeros();
+        //Seteos de los objetos antes mencionados
+        pasajero.setNombre("Mireya");
+        pasajero.setApellido("Suarez");
+        pasajero2.setNombre("Martina");
+        pasajero2.setApellido("García");
+        pasajero3.setNombre("Maria");
+        pasajero3.setApellido("Carvajal");
+        pasajero4.setNombre("Margarita");
+        pasajero4.setApellido("Cadena");
+        pasajero5.setNombre("Pedro");
+        pasajero5.setApellido("Cabrera");
+        //Mostrar
+        pasajero.mostrar();
+        pasajero2.mostrar();
+        pasajero3.mostrar();
+        pasajero4.mostrar();
+        pasajero5.mostrar();
+        //Objetos con valores quemados
+        ticket pasajero1 = new ticket();
+        ticket pasajero2 = new ticket();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
